369aec072715f23a3c683c082f216096247f2fce
A. W. Fuchs <github@andrewfuchs.com>
2017-09-20 00:45:47 -0700

    Create cellar_handler.ino

---
+++ cellar_handler.ino
@@ -0,0 +1,80 @@
+#include "DHT.h"
+
+#define DHTPIN 3     // what digital pin we're connected to
+#define TARGET_TEMP 19
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+int numSamples=0;
+bool fanOn=false;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(5000);
+  numSamples += 1;
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  if( t<=TARGET_TEMP ) {
+    fanOn=false;
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else {
+    fanOn=true;
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  if( fanOn==true ) {
+    Serial.print("OPERATING -- ");
+  } else {
+    Serial.print("IDLE -- ");
+  }
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.println(" *F");
+}
