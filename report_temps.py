3fa252a49f991389a475d4abbd22113a471e2cfe
Andy <andy@Patricia-Mulhollands-MacBook-Air-4.local>
2017-09-20 18:46:06 -0700

    Locally tweaked and modified version: this is the working baseline.

--- report_temps.py
+++ report_temps.py
@@ -1,11 +1,11 @@
-import serial, time
+import serial, time, numpy
+from logtemps import log_temperature_data
 
-# PORT = "/dev/tty.usbserial-DA01R30E"
+
 PORT = "/dev/tty.usbmodem621"
 BAUD = 9600
 
-RPT_INTERVAL = 300 # seconds
-DC_WINDOW = 3 # intervals
+RPT_INTERVAL = 600 # seconds
 
 # incoming data field definitions
 
@@ -18,9 +18,16 @@ ser = serial.Serial(PORT, BAUD)
 # Initialize all the counters and flags
 
 lastReport = 0
+prevMinute = ''
 fanOns     = 0
 fanOffs    = 0
 fanOnOffs  = 0
+maxTemp    = 0
+minTemp    = 99
+allTemp    = []
+DC         = 0
+goalTempSpec ='19.00'
+goalTempAdap ='19.00'
 
 
 while 1:
@@ -33,21 +40,44 @@ while 1:
     # --- set up the current time info ---
     rawTime = time.time()
     theTime = time.localtime(rawTime)
-    displayTime = time.strftime("%Y.%m.%d %H:%M")
+    displayDate = time.strftime("%b %d, %Y")
+    displayTime = time.strftime("%H:%M")
+    currentMinute  = time.strftime("%M")
+    print("%s Temp: %s C, fan %s (prev duty cycle %.0f%%)" % (displayTime, data[CELSIUS], data[FAN], DC*100))
     if data[FAN] != "IDLE":
       fanOns += 1
     else:
       fanOffs += 1
-    if rawTime > lastReport+RPT_INTERVAL:
+    # --- keep the min, max, and samples for avgt ---
+    theTemp = float(data[CELSIUS])
+    maxTemp = max(maxTemp, theTemp)
+    minTemp = min(minTemp, theTemp)
+    allTemp.append(theTemp)
+
+    modMinutes = int(currentMinute) % int(RPT_INTERVAL/60)
+    #if rawTime > lastReport+RPT_INTERVAL:
+    if (prevMinute != currentMinute) and (modMinutes == 0):
+      prevMinute = currentMinute
+      print("Reporting")
       fanOnOffs = fanOns + fanOffs
       if fanOnOffs > 0:
         DC = fanOns / float(fanOnOffs)
-        # print(displayTime + " Temp: "+ str(data[CELSIUS]) +" C, fan is "+ data[FAN] +", duty-cycle "+DC)
-        print("%s Temp: %s C, fan %s (prev 15 min duty cycle %.0f%%)" % (displayTime, data[CELSIUS], data[FAN], DC*100))
       else:
-        print(displayTime + " Temp: "+ str(data[CELSIUS]) +" C, fan is "+ data[FAN] +", duty-cycle (n/a)")
-      #print( fanOns, fanOffs, fanOnOffs, DC )
+        DC = 1
+
+      avgTemp = numpy.mean(allTemp)
+
+      #print("%s %s Temps: min %s, max %s, avg %s, fan duty cycle %.0f%%" % (displayDate, displayTime, minTemp, maxTemp, avgTemp, DC*100))
+
+      log_temperature_data( displayDate, displayTime, goalTempSpec, goalTempAdap, avgTemp, minTemp, maxTemp, DC )
+
+      # --- reset accumulators ---
       lastReport = rawTime 
       fanOns  = 0
       fanOffs = 0
       fanOnOffs = 0
+      maxTemp = theTemp
+      minTemp = theTemp
+      allTemp = []
+
+
