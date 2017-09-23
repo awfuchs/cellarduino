a766303c43d3024cdf05a710ec6c088f800e785a
A. W. Fuchs <github@andrewfuchs.com>
2017-09-22 21:11:42 -0700

    Update cellar_handler.ino

--- cellar_handler.ino
+++ cellar_handler.ino
@@ -6,28 +6,30 @@
 
 #define DHTPIN 3     // what digital pin we're connected to
 #define TARGET_TEMP 18.5
+#define NEVER_EXCEED 25         // Don’t ever adapt higher than 77*F
 
-// Uncomment whatever type you're using!
+#define USEC_PER_SAMPLE 5000    // Five seconds per sample
+#define SAMPLES_PER_PERIOD 120  // Ten minute evaluation period
+
+// Set the DHT type: uncomment whatever type you're using!
 //#define DHTTYPE DHT11   // DHT 11
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
 // Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
 
 int numSamples=0;
 bool fanOn=false;
 
+// vars for adaptive temperature and other duty-cycle driven things
+float adapTemp = TARGET_TEMP;
+int onesPerPeriod = [0, 0, 0, 0]; // Duty cycle counter per period
+int period = 0;                   // Counts 0..3 and repeat
+int samplesThisPeriod = 0;        // Counts 0..119 and repeats
+
+
+// =========== The Arduino setup function ===========
 void setup() {
   Serial.begin(9600);
   Serial.println("DHTxx test!");
@@ -39,11 +41,49 @@ void setup() {
   dht.begin();
 }
 
+// =========== The Arduino loop ==============
 void loop() {
-  // Wait a few seconds between measurements.
-  delay(5000);
+  
+  // Wait for next sample interval, then bump counters...
+  
+  delay(USEC_PER_SAMPLE);
   numSamples += 1;
+  samplesThisPeriod +=1;
+  if (fanOn == true) {
+    onesPerPeriod[period] += 1;
+  }
 
+// ...handle duty cycle processing...
+
+if (samplesThisPeriod >= SAMPLES_PER_PERIOD) {
+  // --- end of period, time to reckon, adapt, and wrap ---
+
+  // reckon
+  totalOnes =  onesPerPeriod[0];
+  totalOnes += onesPerPeriod[1];
+  totalOnes += onesPerPeriod[2];
+  totalOnes += onesPerPeriod[3];
+
+  
+  // adapt
+  if (totalOnes > SAMPLES_PER_PERIOD && adapTemp <= NEVER_EXCEED) {
+    // --- More than 25% duty cycle over 4 periods, increase temp  ---
+    adapTemp += 0.1;
+  }
+  else if (totalOnes < SAMPLES_PER_PERIOD/2) {
+    if (adapTemp > TARGET_TEMP) {
+      // --- Less than 12.5% duty cycle, if above target temp, reduce temp ---
+      adapTemp -= 0.1;
+    }
+  }
+  // wrap
+  period = (period + 1) % 4;  // Increment the period 0,1,2,3,0,1,2,3...
+  samplesThisPeriod = 0;
+  onesPerPeriod[period] = 0;
+}
+
+  //...then take readings...
+  
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
@@ -58,37 +98,20 @@ void loop() {
     return;
   }
 
-  if( t<=TARGET_TEMP ) {
-    fanOn=false;
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-  else {
-    fanOn=true;
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
+  // ...adjust the cooling...
+  
+  if( t<=adapTemp ) { fanOn=false; digitalWrite(LED_BUILTIN, LOW); }
+  else              { fanOn=true; digitalWrite(LED_BUILTIN, HIGH); }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  if( fanOn==true ) {
-    Serial.print("OPERATING -- ");
-  } else {
-    Serial.print("IDLE -- ");
-  }
-  //Serial.print("Humidity: ");
-  //Serial.print(h);
-  //Serial.print(" %\t");
-  //Serial.print("Temperature: ");
+  // ...and finally report data.
+  
+  if( fanOn==true ) { Serial.print("OPERATING -- "); }
+  else              { Serial.print("IDLE -- "); }
+  
   Serial.print(t);
   Serial.print(" *C ");
   Serial.print(f);
-  Serial.println(" *F");
-  // Serial.print(" *F\t");
-  // Serial.print("Heat index: ");
-  // Serial.print(hic);
-  // Serial.print(" *C ");
-  // Serial.print(hif);
-  // Serial.println(" *F");
+  Serial.print(" *F");
+  Serial.print(h);
+  Serial.println("%");
 }
